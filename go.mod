494a5101ed0bf9d6d83560073d4d203019feb919
raja.dodla@ragasedu.com <raja.dodla@ragasedu.com>
2023-09-29 20:51:43 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajsekhar/mylogger
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package mylogger
+
+import "fmt"
+
+func LogInfo() {
+	fmt.Println("this is a info log")
+}
+func LogWarn() {
+	fmt.Println("this is a warn log")
+}
+func LogError() {
+	fmt.Println("this is a error log")
+}
